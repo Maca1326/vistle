186a1be0b4f9bda564227f68ac60dfe302370033
Martin Aumüller <aumuell@reserv.at>
2014-05-06 19:17:52 +0200

    NO COMMIT: vistle_spawn.sh for viscluster

--- vistle/bin/spawn_vistle.sh
+++ vistle/bin/spawn_vistle.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
 
-#echo mpirun "$@"
-#exec mpirun -np 1 "$@"
-echo EXEC: "$@"
-exec mpirun -np 2 "$@"
-#exec xterm -e gdb --args "$@"
-exec "$@"
+case $(hostname) in
+   viscluster*)
+      echo spawn_vistle.sh "$@"
+      HOSTS=$(echo viscluster{50..60} viscluster{71..75}|sed -e 's/ /,/g')
+      exec mpirun -np 16 -H ${HOSTS} "$@"
+      ;;
+   *)
+      #echo mpirun "$@"
+      #exec mpirun -np 1 "$@"
+      echo EXEC: "$@"
+      exec mpirun -np 2 "$@"
+      #exec xterm -e gdb --args "$@"
+      exec "$@"
+      ;;
+esac
